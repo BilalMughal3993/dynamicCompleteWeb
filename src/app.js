d57c69bde2d2653bdbb6e50e2884bf147a8dc2f9
BilalMughal3993 <98820231+BilalMughal3993@users.noreply.github.com>
2022-05-14 15:52:05 +0500

    create navbar component and ad css in this project

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express =require("express");
 const path = require("path");
 
+const hbs = require("hbs");
+
 const app=express();
 
 require("./db/conn");
@@ -10,16 +12,31 @@ const port = process.env.PORT||3000;
 
 //setting the path 
 const staticpath = path.join(__dirname,"../public")
+const templatepath = path.join(__dirname,"../templates/views")
+const partialpath = path.join(__dirname,"../templates/partials")
 
 //middleware for path
+app.use('/css',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css")));
+app.use('/js',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")));
+app.use('/jq',express.static(path.join(__dirname,"../node_modules/jquery/dist")));
+
 app.use(express.static(staticpath))
+
+app.set("view engine","hbs");
+app.set("views",templatepath)
+
+hbs.registerPartials(partialpath);
 
 //ROUTING
 //app.get(path,callback)
 
 app.get("/",(req,res)=>{
-    res.send("Hello i am response")
+    res.render("index")
 })
+app.get("/contact",(req,res)=>{
+    res.render("contact")
+})
+
 
 //server Create 
 
